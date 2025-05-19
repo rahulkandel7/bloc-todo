2588dec9d22e70dba2f043a305573dee279f510c
rahulkandel7 <rahulkandel7@gmail.com>
2025-05-19 09:42:33 +0545

    complete the add todo screen form

--- lib/features/home/presentation/screens/add_todo_screen.dart
+++ lib/features/home/presentation/screens/add_todo_screen.dart
@@ -8,6 +8,10 @@ class AddTodoScreen extends StatefulWidget {
 }
 
 class _AddTodoScreenState extends State<AddTodoScreen> {
+  final GlobalKey<FormState> addTodoForm = GlobalKey();
+
+  String? title;
+  String? description;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,50 +21,85 @@ class _AddTodoScreenState extends State<AddTodoScreen> {
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
         child: Form(
+            key: addTodoForm,
             child: Column(
-          spacing: 20,
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text("Title"),
-            TextFormField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: "Write the title",
-                label: Text("Title"),
-                floatingLabelBehavior: FloatingLabelBehavior.never,
-              ),
-            ),
-            Text("Description"),
-            TextFormField(
-              minLines: 6,
-              maxLines: 10,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: "Write the description",
-                label: Text("Description"),
-                floatingLabelBehavior: FloatingLabelBehavior.never,
-              ),
-            ),
-            Row(
               spacing: 20,
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                FilledButton.tonal(
-                  onPressed: () {},
-                  child: Text("Add"),
-                ),
-                FilledButton.tonal(
-                  onPressed: () {},
-                  child: Text(
-                    "Cancel",
-                    style: TextStyle(color: Colors.white),
+                Text("Title"),
+                TextFormField(
+                  onSaved: (newValue) {
+                    setState(() {
+                      title = newValue;
+                    });
+                  },
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Provide title";
+                    } else {
+                      return null;
+                    }
+                  },
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "Write the title",
+                    label: Text("Title"),
+                    floatingLabelBehavior: FloatingLabelBehavior.never,
                   ),
-                  style: FilledButton.styleFrom(backgroundColor: Colors.red),
+                ),
+                Text("Description"),
+                TextFormField(
+                  onSaved: (newValue) {
+                    setState(() {
+                      description = newValue;
+                    });
+                  },
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Provide description";
+                    } else {
+                      return null;
+                    }
+                  },
+                  minLines: 6,
+                  maxLines: 10,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "Write the description",
+                    label: Text("Description"),
+                    floatingLabelBehavior: FloatingLabelBehavior.never,
+                  ),
+                ),
+                Row(
+                  spacing: 20,
+                  children: [
+                    FilledButton.tonal(
+                      onPressed: () {
+                        if (!addTodoForm.currentState!.validate()) {
+                          return;
+                        }
+                        addTodoForm.currentState!.save();
+                        final Map<String, dynamic> formData = {
+                          'title': title,
+                          'description': description
+                        };
+                      },
+                      child: Text("Add"),
+                    ),
+                    FilledButton.tonal(
+                      onPressed: () {},
+                      child: Text(
+                        "Cancel",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      style:
+                          FilledButton.styleFrom(backgroundColor: Colors.red),
+                    )
+                  ],
                 )
               ],
-            )
-          ],
-        )),
+            )),
       ),
     );
   }
